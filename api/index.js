00cf63bdc995363b2d6aef89f6a586b1caa3efba
pro-aline <aline.balogh@etec.sp.gov.br>
2025-09-05 13:50:12 -0300

    add new route with filter by id

--- api/index.js
+++ api/index.js
@@ -25,6 +25,23 @@ app.get('/monstros', (req, res) => {
     res.json(monstros);
 });
 
+// Rota GET para retornar um monstro by ID
+app.get('/monstros/:monstro_id', (req, res) => {
+
+    // Descubra como pegar o ID passado pela URL através do atributo req.params 
+    let id = req.params._____;
+    // Pesquise sobre o método find em javascript e filtre o monstro por ID.
+    // Vale 15 pts
+    let monstro = ________________ ;
+
+    if (monstro) {
+        res.json(monstro)
+    } else {
+        res.status(404).json({ erro: 'Nenhum monstro encontrado.' });
+    }
+});
+
+
 // --- Iniciar o Servidor ---
 
 // Faz o aplicativo Express começar a "escutar" por requisições na porta definida.

28e908098204bdd2e6e8b6f8b48eaa48c4e9150d
Aline Balogh <aline.balogh@gmail.com>
2025-09-05 13:52:20 -0300

    Revert "GitHub Classroom: Sync Assignment"

--- api/index.js
+++ api/index.js
@@ -56,23 +56,6 @@ if (monstros.length > 0) {
 }
 });
 
-// Rota GET para retornar um monstro by ID
-app.get('/monstros/:monstro_id', (req, res) => {
-
-    // Descubra como pegar o ID passado pela URL através do atributo req.params 
-    let id = req.params._____;
-    // Pesquise sobre o método find em javascript e filtre o monstro por ID.
-    // Vale 15 pts
-    let monstro = ________________ ;
-
-    if (monstro) {
-        res.json(monstro)
-    } else {
-        res.status(404).json({ erro: 'Nenhum monstro encontrado.' });
-    }
-});
-
-
 // --- Iniciar o Servidor ---
 
 // Faz o aplicativo Express começar a "escutar" por requisições na porta definida.
